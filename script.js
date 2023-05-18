c8359b04959d9a71a60252d1cd1afa6b5ab2f304
Shippo704 <matt.taylor704@yahoo.ca>
2023-05-18 09:39:08 -0400

    added comments to js code

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ function writePassword() {
 
 }
 
-// Generate Password
+// Generate Password Function
 function generatePassword() {
   var passLength = 0;
   var check = false;
 
-  // Get the length of the password
+  //------------------------------------------------
+  // User Input to declare variables
+  //------------------------------------------------
+
+  // Get the length of the password from user
   while (passLength < 8 || passLength > 128 || check) {
     passLength = prompt("How many characters will your password have? (8-128)");
     //error check if non-number is entered into prompt box
@@ -24,13 +28,14 @@ function generatePassword() {
 
   // What characters to use in prompts
   var numCat = 0;
+  //at least 1 character class must be selected
   while (numCat <1){
     var lower = confirm("Will your password contain lowercase characters? (OK for yes, Cancel for no)");
     var upper = confirm("Will your password contain uppercase characters? (OK for yes, Cancel for no)");
     var number = confirm("Will your password contain numbers? (OK for yes, Cancel for no)");
     var special = confirm("Will your password contain special characters? (OK for yes, Cancel for no)");
 
-    // Determine number of character classes
+    // Determine number of character classes from user input
     if (lower) {
       numCat++;
     }
@@ -45,6 +50,10 @@ function generatePassword() {
     }
   }
 
+  //-------------------------------------------------------------------------
+  // Generate an array containing random characters according to user input
+  //-------------------------------------------------------------------------
+
   // Declare array to save randomly generated characters
   var charArray = [];
   var charTracker = passLength;
@@ -52,17 +61,22 @@ function generatePassword() {
   //Lowercase characters
   if (lower == true) {
     var numLower = 0;
+    //if lowercase is the last/only category selected then all remaining characters will be lowercase
     if (numCat == 1) {
       numLower = charTracker;
     }
+    //generate a random number of lowercase characters
+    //leave at least 1 possible character for each remaining character class
     else{
       numLower = Math.floor((Math.random())*(charTracker-numCat)+1);
     }
+    //add lowercase characters to an array that stores all the characters
     var lChar = "";
     for (i=0; i<numLower; i++) {
       lChar = String.fromCharCode(Math.floor((Math.random()*26)+97));
       charArray.push(lChar);
     }
+    //update tracker/counter variables
     charTracker = charTracker-numLower;
     numCat--;
   }
@@ -70,17 +84,22 @@ function generatePassword() {
   //Uppercase Characters
   if (upper == true) {
     var numUpper = 0;
+    //if uppercase is the last/only category selected then all remaining characters will be uppsercase
     if (numCat ==1) {
       numUpper = charTracker;
     }
+    //generate a random number of uppercase characters
+    //leave at lease 1 possible character for each remaining character class
     else{
       numUpper = Math.floor((Math.random())*(charTracker-numCat)+1);
     }
+    //add uppercase characters to an array that stores all the characters
     var uChar = "";
     for (i=0; i<numUpper; i++) {
       uChar = String.fromCharCode(Math.floor((Math.random()*26)+65));
       charArray.push(uChar);
     }
+    //update tracker/counter variables
     charTracker = charTracker-numUpper;
     numCat--;
   }
@@ -88,25 +107,32 @@ function generatePassword() {
  //Numbers
  if (number == true) {
   var numNum = 0;
+  //if number is the last/only category selected then all remaining characters will be numbers
   if (numCat == 1) {
     numNum = charTracker;
   }
+  //generate a random number of number characters
+  //leave at least 1 possible character for each remaining category
   else {
     var numNum = Math.floor((Math.random())*(charTracker-numCat)+1);
   }
+  //add numbers to an array that stores all variables
   var nChar = "";
   for (i=0; i<numNum; i++) {
     nChar = String.fromCharCode(Math.floor((Math.random()*10)+48));
     charArray.push(nChar);
   }
+  //update tracker/counter variables
   charTracker = charTracker-numNum;
   numCat--;
 }
  
   //Special Characters
   if (special == true) {
+    //special characters will always be the last category
     var numSpec = charTracker;
     var sChar = "";
+    //generate random special characters from ASCII values
     for (i=0; i<numSpec; i++) {
       sChar = Math.floor(Math.random()*15);
       //ASCII range 33-47
@@ -121,12 +147,14 @@ function generatePassword() {
       else {
         sChar = String.fromCharCode(sChar + 69);
       }
-
+      //add special characters to an array that stores all characters
       charArray.push(sChar);
     }
   }
 
-  //Generate password randomly from array
+  //--------------------------------------------------------------
+  // Generate password randomly from array of random characters
+  //--------------------------------------------------------------
   
   //Generate matching index array
   var indexArr = [];
@@ -155,6 +183,7 @@ function generatePassword() {
     pass = pass + charArray[indexArr[i]];
   }
 
+  //return the password so writePassword() can use it
   return pass;
 }
 
